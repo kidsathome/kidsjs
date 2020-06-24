b7e8d17bc1222d56fe639c4f37b75accaf153c72
kidsathome <67339211+kidsathome@users.noreply.github.com>
2020-06-25 00:36:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+  function check(form)
+  {
+  if(form.userEmail.value == "dew" && form.userPass.value == "123" )
+  {
+  
+  window.open("https://wwww.google.com")
+  
+  }
+  else
+  {
+  
+  alert("The Username and password you entered don't match")
+  }
+  
+  
+  
+  }
 
- function login(){
- alert("Hello Javatpoint");  
-}
+ 
